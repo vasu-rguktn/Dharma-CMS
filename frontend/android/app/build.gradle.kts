04677dcb006c2620c1033601bab0662d23c40f78
PradeepSavara <savarapradeep24@gmail.com>
2026-02-06 23:17:51 +0530

    final version 1 Dharma

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -27,17 +27,22 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion  // Required for Firebase
-        targetSdk = 34
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
         multiDexEnabled = true
     }
+    
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
+            // Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+            
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
 }
@@ -46,9 +51,18 @@ flutter {
     source = "../.."
 }
 
+configurations.all {
+    exclude(group = "com.google.android.play", module = "core-common")
+}
+
 dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
     implementation("androidx.multidex:multidex:2.0.1")
-    // implementation(platform("com.google.firebase:firebase-bom:34.4.0"))
+
     implementation("com.google.firebase:firebase-analytics")
+
+    // REQUIRED for Flutter
+    implementation("com.google.android.play:core:1.10.3")
 }
+
+
